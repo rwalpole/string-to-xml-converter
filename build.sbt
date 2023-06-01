e92286cdfe0122a1325454d73dab8bd2ac6c7fde
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2023-06-01 23:37:53 +0100

    Updating dependencies

--- build.sbt
+++ build.sbt
@@ -1,9 +1,9 @@
 name := "string-to-xml-converter"
 
-version := "0.1"
+version := "0.2"
 
-scalaVersion := "2.13.0"
+scalaVersion := "2.13.10"
 
 libraryDependencies ++= Seq(
-  "org.scala-lang.modules" %% "scala-xml" % "1.2.0",
-  "org.scalatest" %% "scalatest" % "3.0.8" % "test")
+  "org.scala-lang.modules" %% "scala-xml" % "2.1.0",
+  "org.scalatest" %% "scalatest" % "3.2.15" % "test")
--- src/test/scala/uk/co/devexe/xml/XmlConversionSpec.scala
+++ src/test/scala/uk/co/devexe/xml/XmlConversionSpec.scala
@@ -1,31 +1,32 @@
 package uk.co.devexe.xml
 
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
 import scala.xml.Text
 
-class XmlConversionSpec extends WordSpec with Matchers {
+class XmlConversionSpec extends AnyWordSpec with Matchers {
 
-  "stringToXmlNodes" should {
+  "stringToXmlNodes" must {
     "return an empty Text node when passed an empty string" in {
       val result = XmlConversion.stringToXmlNodes("")
-      result.get.head shouldBe Text("")
+      result.get.head mustBe Text("")
     }
     "return an empty text node when passed a string containing only a carriage return" in {
       val result = XmlConversion.stringToXmlNodes("\n")
-      result.get.head shouldBe Text("")
+      result.get.head mustBe Text("")
     }
     "return a sequence containing one XML node when passed a valid XML string an single node" in {
       val result = XmlConversion.stringToXmlNodes("<test>test</test>")
-      result.get shouldBe Seq(<test>test</test>)
+      result.get mustBe Seq(<test>test</test>)
     }
     "return a sequence of XML nodes when passed a valid XML string containing multiple nodes" in {
       val result = XmlConversion.stringToXmlNodes("<test>test1</test><test>test2</test>")
-      result.get shouldBe <test>test1</test><test>test2</test>
+      result.get mustBe <test>test1</test><test>test2</test>
     }
     "return a Failure if the XML cannot be parsed" in {
       val result = XmlConversion.stringToXmlNodes("<test>&</test>")
-      result.isFailure shouldBe true
+      result.isFailure mustBe true
     }
   }
 
